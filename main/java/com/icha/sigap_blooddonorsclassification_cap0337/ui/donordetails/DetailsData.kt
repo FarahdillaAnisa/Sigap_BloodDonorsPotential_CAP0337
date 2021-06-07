cb617450ce5f54679f02e806a4fd24f5a83a8d17
BMaverickS <49307098+BMaverickS@users.noreply.github.com>
2021-06-08 00:00:13 +0700

    Delete main directory

--- main/java/com/icha/sigap_blooddonorsclassification_cap0337/ui/donordetails/DetailsData.kt
+++
@@ -1,9 +0,0 @@
-package com.icha.sigap_blooddonorsclassification_cap0337.ui.donordetails
-
-data class DetailsData( // update data list
-    var detailNik : String?,
-    var detailName : String?,
-    var detailGender : String?,
-    var detailBloodType : String?,
-    var detailRhesus : String?
-)
